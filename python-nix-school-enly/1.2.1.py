2552b76ea5873a9949c419ec3d59c63aa4ae8c7c
enly <lydyia.en@nixsolutions.com>
2021-09-16 18:26:05 +0300

    change file 1.2.1

--- python-nix-school-enly/1.2.1.py
+++ python-nix-school-enly/1.2.1.py
@@ -1,30 +1,41 @@
 import re
 
+def find_line(all_lines):
+    pattern_lines = [string.split("eid: ")[1] for string in all_lines if "eid: " in string]
+    needed_lines = pattern_lines[-2:]
+    return needed_lines
 
-def return_dif(file):
-    lines_list = file.readlines()
-    substring = "D:TUpdaterController::SetUniqueParam(429): eid:"
 
-    eids = [string for string in lines_list if substring in string]
-    needed_eids = eids[-2:]
-    pre_last_eid, last_eid = needed_eids
+def decompose_line(some_lines):
+    list_lines = []
+    for line in find_line(some_lines):
+        decomposed_lines = dict(params.split('.') for params in line.split(';'))
+        list_lines.append(decomposed_lines)
+    return list_lines
 
-    def string_to_dict(s):
-        kv = re.findall(r'\w*\.\w*', s)
-        kv_dict = {i.split('.')[0]: i.split('.')[1] for i in kv}
-        return kv_dict
 
-    dict_pre_last = string_to_dict(pre_last_eid)
-    dict_last = string_to_dict(last_eid)
+def compare_params(dict1, dict2):
+    difference_dict = {}
+    for key in dict2:
+        if key in dict1:
+            if dict1[key] != dict2[key]:
+                difference_dict[key] = {'last': dict1[key], 'second_to_last': dict2[key]}
+        else:
+            difference_dict[key] = {'last': 'BLANK', 'second_to_last': dict2[key]}
 
-    values_in_last = {key: dict_last[key] for key in dict(set(dict_last.items()) - set(dict_pre_last.items()))}
-    values_in_pre_last = {key: dict_pre_last[key] for key in dict(set(dict_pre_last.items()) - set(dict_last.items()))}
-    general_dict_of_dif = {'values_in_last': values_in_last, 'values_in_pre_last': values_in_pre_last}
-    return general_dict_of_dif
+    for key in dict1:
+        if key not in dict2 and key not in difference_dict:
+            difference_dict[key] = {'last': dict1[key], 'second_to_last': 'BLANK'}
+
+    return difference_dict
+
+
+def return_result(all_lines):
+    result = decompose_line(all_lines)
+    return f'\nResult:{compare_params(result[0],result[1])}'
 
 
 if __name__ == "__main__":
-    log_file = open('yupdate-exec-yabrowser.log', 'r')
-    print(return_dif(log_file))
-
-    1111
+    with open('yupdate-exec-yabrowser.log', 'r') as log_file:
+        lines = log_file.readlines()
+    print(return_result(lines))
