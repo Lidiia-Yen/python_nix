df28f0a218281ba2859d9519beb42569fb37bbdc
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-09-23 23:09:32 +0300

    change 1.3.1_t2

--- python-nix-school-enly/1.3.1_t2.py
+++ python-nix-school-enly/1.3.1_t2.py
@@ -1,28 +1,17 @@
 from pytz import timezone
-from datetime import datetime, time, date
+from datetime import datetime
 
 
 def time_dif(a, b):
-    timezone_1 = timezone(a)
-    timezone_2 = timezone(b)
-    utc_time1 = datetime.now(timezone_1)
-    utc_time2 = datetime.now(timezone_2)
-    time_1 = time(hour=datetime.utcnow().hour, minute=datetime.utcnow().minute, tzinfo=timezone_1)
-    time_2 = time(hour=datetime.utcnow().hour, minute=datetime.utcnow().minute, tzinfo=timezone_2)
-    dateTime1 = time_1.tzinfo.localize(datetime.combine(date.today(), time_1, tzinfo=None)) #повторно использует
-    # ссылку tzinfo на объект времени time_1 и явно устанавливает tzinfo в None для получения правильного смещения timezone.
-    dateTime2 = time_2.tzinfo.localize(datetime.combine(date.today(), time_2, tzinfo=None))
-    diff = dateTime2 - dateTime1
+    time1 = datetime.now(timezone(a))
+    time2 = datetime.now(timezone(b))
+    if time1.date() == time2.date():
+        dif = abs(time2.hour-time1.hour)
+    else:
+        dif = (24-time1.hour+time2.hour if time1.hour > time2.hour else 24-time2.hour+time1.hour)
 
-    return f'\nTime in city 1: {utc_time1.strftime("%H:%M:%S")}\nTime in city 2: {utc_time2.strftime("%H:%M:%S")}\nTime difference: {diff}'
+    return f'\nTime in city 1: {time1.strftime("%X, %d %b %Y")}\nTime in city 2: {time2.strftime("%X, %d %b %Y")}\nTime difference: {dif} hour(s)'
 
 
 if __name__ == '__main__':
-    city1 = str(input())
-    city2 = str(input())
-    print(time_dif(city1, city2))
-
-
-#Input Example:
-#Turkey
-#Europe/Amsterdam
+    print(time_dif('Canada/Eastern', 'Australia/Melbourne'))
