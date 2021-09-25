df6b9eaf5d85f7690b95a725a33555b4b29a1983
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-09-25 15:53:56 +0300

    change files 1.2-1.3

--- python-nix-school-enly/1.3.1_t3.py
+++ python-nix-school-enly/1.3.1_t3.py
@@ -1,35 +1,36 @@
 from datetime import datetime, date
 from collections import namedtuple
 
-if __name__ == '__main__':  # оч корявая структура файла и уровни вложенности. Обрати внимание на замечания иде и
-    # переделай
-    i = str(input())
-try:
-    dt_start = datetime.strptime(i, '%d-%m-%Y')
-except ValueError:
-    print("Incorrect format")
-
 
 zodiacs = [(120, 'Cap'), (218, 'Aqu'), (320, 'Pis'), (420, 'Ari'), (521, 'Tau'),
            (621, 'Gem'), (722, 'Can'), (823, 'Leo'), (923, 'Vir'), (1023, 'Lib'),
            (1122, 'Sco'), (1222, 'Sag'), (1231, 'Cap')]
 
 
-def get_zodiac_of_date(date):
-    date_number = int("".join((str(date.date().month), '%02d' % date.date().day)))
+def get_zodiac_of_date(date_of_birth):
+    date_number = int("".join((str(date_of_birth.date().month), '%02d' % date_of_birth.date().day)))
     for z in zodiacs:
         if date_number <= z[0]:
             return z[1]
 
 
-def calculate_age(born):
+def calculate_age(date_of_birth):
     today = date.today()
-    return today.year - born.year - ((today.month, today.day) < (born.month, born.day)) #comparison returns True = 1 or False = 0
+    return today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
+    #comparison returns True = 1 or False = 0
 
 
-zodiac = get_zodiac_of_date(dt_start)
-age = calculate_age(dt_start)
-Person = namedtuple('Person', ['zod', 'age'])
-response = Person(zodiac, age)
+def return_result(date_of_birth):
+    zodiac = get_zodiac_of_date(date_of_birth)
+    age = calculate_age(date_of_birth)
+    Person = namedtuple('Person', ['zod', 'age'])
+    return Person(zodiac, age)
 
-print(response)
+
+if __name__ == '__main__':
+    input_date = str(input())
+try:
+    birthday = datetime.strptime(input_date, '%d-%m-%Y')
+    print(return_result(birthday))
+except ValueError:
+    print("Incorrect format, please enter dd-mm-yyyy")
