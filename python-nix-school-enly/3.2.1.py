8429897a241faae80ee7e961ef2c13da7cef4fb4
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-10-19 00:59:10 +0300

    change functions

--- python-nix-school-enly/3.2.1.py
+++ python-nix-school-enly/3.2.1.py
@@ -1,47 +1,70 @@
 import logging
 import re
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-def log_errors(myfile):  # в одной функции у тебя описана куча всего не связанного, как минимум логгер точно должен
-    # быть либо отдельным классом, либо описан на уровне модуля. У функции должно быть 1 назначение, у тебя их тут 5
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.ERROR)
-    handler = logging.FileHandler('found_errors.log')
-    handler.setLevel(logging.ERROR)
-    console = logging.StreamHandler()
-    console.setLevel(logging.CRITICAL)
-    logger.addHandler(handler)
-    logger.addHandler(console)
-    q_errors = 0  # не понятное имя переменной
-    lines = 0
-    with open(myfile, 'r') as f:
-        try:
-            for line in f:
-                lines += 1  # пересмотри структуру лога, такой алгоритм даст не верный результат
-                error = len(re.findall(r'ERROR|Error|error', line))  # подумай, что ты здесь считаешь, и что нужно считать по заданию
-                q_errors += error
-                critical = len(re.findall(r'CRITICAL|Critical|critical', line))
-                if error != 0:  # не очень хорошее решение привязываться к количеству для вывода лога, здесь лучше по результатам поиска
-                    logger.error(line)
-                    if critical != 0:
-                        logger.critical(line)
-                        try:
-                            raise CriticalError(line)
+file_handler = logging.FileHandler('found_errors.log')
+file_handler.setLevel(logging.ERROR)
+logger.addHandler(file_handler)
 
-                        except CriticalError as e:
-                            print('It is a critical error ' + str(e))
+console = logging.StreamHandler()
+console.setLevel(logging.CRITICAL)
+logger.addHandler(console)
 
-        except ZeroDivisionError:
-            print('You cannot divide by zero!')
-        finally:
-            k = lines / q_errors
-            return k
+# в одной функции у тебя описана куча всего не связанного, как минимум логгер точно должен
+# быть либо отдельным классом, либо описан на уровне модуля. У функции должно быть 1 назначение, у тебя их тут 5
 
-
-
-class CriticalError(Exception):  # используй автоформат, обращай внимание на замечания иде
+class CriticalError(Exception):
     pass
 
+
+def count_record(lines):
+    records = 0
+    record_pattern = re.compile('^\\d\\d:\\d\\d:\\d\\d')
+    for line in lines:
+        if record_pattern.findall(line):
+            records += 1
+    return records
+
+
+def count_errors(lines):
+    errors = 0
+    error_pattern = re.compile('ERROR|Error|error')
+    critical_pattern = re.compile('.*CRITICAL ERROR*.')
+    for line in lines:
+        if error_pattern.findall(line):
+            errors += 1
+            logger.error(line)
+        try:
+            if critical_pattern.findall(line):
+                raise CriticalError(line)
+        except CriticalError:
+            logger.critical(line)
+
+    return errors
+
+
+def divide_lines_to_errors(records, errors):
+    try:
+        division = records/errors
+    except ZeroDivisionError as e:
+        return 0
+    else:
+        return division
+
+
+def read_file(file):
+    try:
+        with open(file) as log_file:
+            lines = log_file.readlines()
+    except Exception as e:
+        print(e)
+    else:
+        records = count_record(lines)
+        errors = count_errors(lines)
+    return f'(количество записей в yupdate.log) / (количество записей с ошибками) = {divide_lines_to_errors(records, errors)} '
+
+
 if __name__ == "__main__":
-    a = log_errors(myfile='yupdate.log')
-    print(a)
+    print(read_file('yupdate.log'))
