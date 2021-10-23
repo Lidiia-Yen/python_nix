324968ca387fb8cbd7580c5a6ed67d38e9913a6e
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-10-23 22:15:04 +0300

    add module, decompose to functions

--- python-nix-school-enly/3.2.1.py
+++ python-nix-school-enly/3.2.1.py
@@ -1,19 +1,6 @@
-import logging
 import re
+import my_logger
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-file_handler = logging.FileHandler('found_errors.log')
-file_handler.setLevel(logging.ERROR)
-logger.addHandler(file_handler)
-
-console = logging.StreamHandler()
-console.setLevel(logging.CRITICAL)
-logger.addHandler(console)
-
-# в одной функции у тебя описана куча всего не связанного, как минимум логгер точно должен
-# быть либо отдельным классом, либо описан на уровне модуля. У функции должно быть 1 назначение, у тебя их тут 5
 
 class CriticalError(Exception):
     pass
@@ -21,7 +8,7 @@ class CriticalError(Exception):
 
 def count_record(lines):
     records = 0
-    record_pattern = re.compile('^\\d\\d:\\d\\d:\\d\\d')
+    record_pattern = re.compile('[0-9]{2}:[0-9]{2}:[0-9]{2}')
     for line in lines:
         if record_pattern.findall(line):
             records += 1
@@ -30,26 +17,27 @@ def count_record(lines):
 
 def count_errors(lines):
     errors = 0
-    error_pattern = re.compile('ERROR|Error|error')
-    critical_pattern = re.compile('.*CRITICAL ERROR*.')
+    error_pattern = re.compile('.*Error.*|.*error.*|.*ERROR.*')
+    critical_pattern = re.compile('.*CRITICAL ERROR.*')
     for line in lines:
-        if error_pattern.findall(line):
-            errors += 1
-            logger.error(line)
         try:
             if critical_pattern.findall(line):
                 raise CriticalError(line)
         except CriticalError:
-            logger.critical(line)
-
+            my_logger.logger.critical(line)
+            errors += 1
+        else:
+            if error_pattern.findall(line):
+                errors += 1
+                my_logger.logger.error(line)
     return errors
 
 
-def divide_lines_to_errors(records, errors):
+def divide_lines_to_errors(record, error):
     try:
-        division = records/errors
+        division = record/error
     except ZeroDivisionError as e:
-        return 0
+        return e
     else:
         return division
 
@@ -63,7 +51,7 @@ def read_file(file):
     else:
         records = count_record(lines)
         errors = count_errors(lines)
-    return f'(количество записей в yupdate.log) / (количество записей с ошибками) = {divide_lines_to_errors(records, errors)} '
+        return f'\n(количество записей в yupdate.log) / (количество записей с ошибками) = {divide_lines_to_errors(records, errors)}'
 
 
 if __name__ == "__main__":
