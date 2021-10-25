e1fbb943e963727b4ba922f401364b753fd0e47e
enly <lydyia.en@nixsolutions.com>
2021-10-25 18:24:53 +0300

    change dec_trace

--- python-nix-school-enly/4.1.1.py
+++ python-nix-school-enly/4.1.1.py
@@ -2,46 +2,46 @@ import datetime
 
 test = input()
 
-def dec_tras(arg, file_name='C:/Users/enly/function_errors.log'):  # автоформат + не рабочий скрипт
+
+def dec_trace(arg, file_name='function_errors.log'):
     def outer(func):
         def inner(*args, **kwargs):
             resp = func(*args, **kwargs)
-            file = open(file_name, mode='a', encoding='utf-8')  # ты уже ранее использовала менеджер контекста,
-            # почему не использовала его здесь?
-            file_content = (
-                    "[time]:" + str(datetime.datetime.now().strftime("%H:%M:%S")) +
-                    " [module]: " + str(format(func.__module__)) +
-                    " [function]: " + str(format(func.__name__)) +
-                    " [arguments]: " + str(arg) +
-                    " [return]: " + str(resp))  # почитай про f-стринги и почти всегда юзай их
-            file.write(str(file_content) + '\n')  # почему не сделать это в предыдущей строке?
-            file.close()
+            with open(file_name, mode='a', encoding='utf-8') as file:
+                file.write(
+                    f'[time]:{datetime.datetime.now().strftime("%H:%M:%S")},[module]:{format(func.__module__)},['
+                    f'function]:{format(func.__name__)},[arguments]:{arg}, [return]:{resp}\n')
+                file.close()
             return resp
+
         return inner
+
     return outer
 
 
-def decorate(func):
-    def wrapper(list):  # плохое имя, тем более не лист
+def decorate(function):
+    def wrapper(word):  # плохое имя, тем более не лист
         result = []
-        dict = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'ye', 'ж': 'zh', 'з': 'z',
+        letters_dict = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'ye', 'ж': 'zh', 'з': 'z',
                 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
                 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
                 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}  # плохое имя, не отражает сущности
         # + конфликт
-        for i in list:
-            if i in dict:  # ты могла бы избавиться от одного из стейтментов
-                result.append(dict[i])
-            elif str(i).lower() in dict:  # зачем здесь явное преобразование?
-                result.append(str(dict[str(i).lower()]).upper())  # зачем здесь явное преобразование?
+        for i in word:
+            if i in letters_dict: # ты могла бы избавиться от одного из стейтментов
+                result.append(letters_dict[i])
+            elif str(i).lower() in letters_dict:  # зачем здесь явное преобразование?
+                result.append(str(letters_dict[str(i).lower()]).upper())  # зачем здесь явное преобразование?
                 # + очень громоздкая конструкция
             else:
                 result.append(i)
-        return func(result)
+        return function(result)
+
     return wrapper
 
 
-@dec_tras(test)
+
+@dec_trace(test)
 @decorate
 def print_func(arg):
     return ''.join(list(arg))  # зачем преобразование? В пайтоне явные преобразования практически не нужны,
