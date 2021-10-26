f9d849f104bd4f677db72f791e5b0ebdc7c2f5bb
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-10-26 23:21:43 +0300

    add module, decompose to functions

--- python-nix-school-enly/4.1.1.py
+++ python-nix-school-enly/4.1.1.py
@@ -1,18 +1,18 @@
 import datetime
 
-test = input()
+test = list(map(str, input()))
 
 
 def dec_trace(arg, file_name='function_errors.log'):
     def outer(func):
         def inner(*args, **kwargs):
-            resp = func(*args, **kwargs)
+            result = func(*args, **kwargs)
             with open(file_name, mode='a', encoding='utf-8') as file:
                 file.write(
-                    f'[time]:{datetime.datetime.now().strftime("%H:%M:%S")},[module]:{format(func.__module__)},['
-                    f'function]:{format(func.__name__)},[arguments]:{arg}, [return]:{resp}\n')
-                file.close()
-            return resp
+                    f"[time]:{datetime.datetime.now().strftime('%H:%M:%S')},[module]:{format(func.__module__)},["
+                    f"function]:{format(func.__name__)},[arguments]:{''.join(arg)}, [return]:{result}\n")
+
+            return result
 
         return inner
 
@@ -23,20 +23,21 @@ def decorate(function):
     def wrapper(word):
         result = []
         letters_dict = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'ye', 'ж': 'zh', 'з': 'z',
-                'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-                'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
-                'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+                        'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+                        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+                        'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
 
-        for i in word:
-            if i.isupper() is True:
-                result.append(letters_dict[i.lower()].capitalize())
-            else:
-                result.append(letters_dict[i.lower()])
+        for letter in word:
+            if letter.isupper():
+                result.append(letters_dict[letter.lower()].capitalize())
+            elif letter.islower():
+                result.append(letters_dict[letter.lower()])
+            elif not letter.isalpha():
+                result.append(letter)
 
         return function(result)
 
     return wrapper
-
 
 
 @dec_trace(test)
@@ -46,4 +47,4 @@ def print_func(arg):
 
 
 if __name__ == '__main__':  # разберись со значением этой конструкции, потому что структура модуля кривая
-    print(print_func(test))  # работает не совсем корректно, попробуй транслитирировать "Щур"
+    print(print_func(test))
