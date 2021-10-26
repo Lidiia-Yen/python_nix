8e04cc5d6fb28ae36f798b5a9f98972d0bf26657
enly <lydyia.en@nixsolutions.com>
2021-10-26 18:26:05 +0300

    change wrappers

--- python-nix-school-enly/4.1.1.py
+++ python-nix-school-enly/4.1.1.py
@@ -20,21 +20,19 @@ def dec_trace(arg, file_name='function_errors.log'):
 
 
 def decorate(function):
-    def wrapper(word):  # плохое имя, тем более не лист
+    def wrapper(word):
         result = []
         letters_dict = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'ye', 'ж': 'zh', 'з': 'z',
                 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
                 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
-                'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}  # плохое имя, не отражает сущности
-        # + конфликт
+                'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+
         for i in word:
-            if i in letters_dict: # ты могла бы избавиться от одного из стейтментов
-                result.append(letters_dict[i])
-            elif str(i).lower() in letters_dict:  # зачем здесь явное преобразование?
-                result.append(str(letters_dict[str(i).lower()]).upper())  # зачем здесь явное преобразование?
-                # + очень громоздкая конструкция
+            if i.isupper() is True:
+                result.append(letters_dict[i.lower()].capitalize())
             else:
-                result.append(i)
+                result.append(letters_dict[i.lower()])
+
         return function(result)
 
     return wrapper
@@ -44,8 +42,7 @@ def decorate(function):
 @dec_trace(test)
 @decorate
 def print_func(arg):
-    return ''.join(list(arg))  # зачем преобразование? В пайтоне явные преобразования практически не нужны,
-    # не делай их абы где
+    return ''.join(arg)
 
 
 if __name__ == '__main__':  # разберись со значением этой конструкции, потому что структура модуля кривая
