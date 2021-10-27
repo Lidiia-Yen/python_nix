1c01fbe9056dd32366e27fa4c2ab2ab565f629ee
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-10-28 00:01:56 +0300

    change init and if __main__

--- python-nix-school-enly/4.2.1.py
+++ python-nix-school-enly/4.2.1.py
@@ -2,21 +2,18 @@ import psycopg2
 
 
 class DataBaseConnector:
-    def __init__(self, db_name: str, host: str, port: str, user: str, password: str, query): # если ты используешь
-        # аннотации - использу их везде
+    def __init__(self, db_name, host, port, user, password):
         self.db_name = db_name
         self.host = host
         self.port = port
         self.user = user
-        self.password = password
-        self.query = query  # плохое решение загнать и запрос и коннект в инит
+        self.password = password  # плохое решение загнать и запрос и коннект в инит
 
     def __enter__(self):
         self.conn = psycopg2.connect(database=self.db_name, user=self.user, password=self.password, host=self.host,
                                      port=self.port)
         self.cursor = self.conn.cursor()
-        self.cursor.execute(self.query)
-        return self.cursor.fetchall()
+        return self.cursor
 
     def __exit__(self, exc_type, esc_val, exc_tb):
         self.cursor.close()
@@ -25,5 +22,7 @@ class DataBaseConnector:
 
 if __name__ == "__main__":
     with DataBaseConnector(db_name='countries', host='10.10.30.252', port='5432', user='postgres',
-                           password='p0S+gre', query='SELECT * FROM "public"."Capital" LIMIT 10') as a:
+                           password='p0S+gre') as a:
+        a.cursor.execute('SELECT * FROM "public"."Capital" LIMIT 10')
+        a.cursor.fetchall()
         print(a)
