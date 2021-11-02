03333010a4a34df20ccca9f65144ee32bd993fac
Lidiia_Yen <lydyia.en@nixsolutions.com>
2021-11-03 00:12:20 +0200

    chamge init and main

--- python-nix-school-enly/4.2.1.py
+++ python-nix-school-enly/4.2.1.py
@@ -7,22 +7,21 @@ class DataBaseConnector:
         self.host = host
         self.port = port
         self.user = user
-        self.password = password  # плохое решение загнать и запрос и коннект в инит
+        self.password = password
 
     def __enter__(self):
         self.conn = psycopg2.connect(database=self.db_name, user=self.user, password=self.password, host=self.host,
                                      port=self.port)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        return self.conn
 
     def __exit__(self, exc_type, esc_val, exc_tb):
-        self.cursor.close()
         self.conn.close()
 
 
 if __name__ == "__main__":
     with DataBaseConnector(db_name='countries', host='10.10.30.252', port='5432', user='postgres',
                            password='p0S+gre') as a:
-        a.cursor.execute('SELECT * FROM "public"."Capital" LIMIT 10')
-        a.cursor.fetchall()
-        print(a)
+        cursor = a.cursor()
+        cursor.execute('SELECT * FROM "public"."Capital" LIMIT 10')
+        print(cursor.fetchall())
+        cursor.close()
