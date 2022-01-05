31e4a5b028260e1ffbb0ef55053ce1c84e53c5b5
Lidiia_Yen <lydyia.en@nixsolutions.com>
2022-01-05 20:00:08 +0200

    update 1.2.1

--- python-nix-school-enly/1.2.1.py
+++ python-nix-school-enly/1.2.1.py
@@ -18,8 +18,9 @@ def decompose_line(all_lines):
 def compare_params(dict_pre_last, dict_last):
     difference_dict = {}
     for key in dict_last:
-        if key in dict_pre_last and dict_pre_last[key] != dict_last[key]:
-            difference_dict[key] = {'pre_last': dict_pre_last[key], 'last': dict_last[key]}
+        if key in dict_pre_last:
+            if dict_pre_last[key] != dict_last[key]:
+                difference_dict[key] = {'pre_last': dict_pre_last[key], 'last': dict_last[key]}
         else:
             difference_dict[key] = {'pre_last': 'BLANK', 'last': dict_last[key]}
 
