7caebd32681859cdad76a3506573a1959aafe3f5
Lidiia_Yen <lydyia.en@nixsolutions.com>
2022-02-19 21:24:09 +0200

    add raise exception,  add execute_query separate method

--- 4/4.2.1.py
+++ 4/4.2.1.py
@@ -10,22 +10,27 @@ class DataBaseConnector:
         self.password = password
 
     def __enter__(self):
-        self.conn = psycopg2.connect(database=self.db_name, user=self.user, password=self.password, host=self.host,
-                                     port=self.port)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = psycopg2.connect(database=self.db_name, user=self.user, password=self.password, host=self.host,
+                                         port=self.port)
+        except psycopg2.Error as err:
+            raise psycopg2.Error(f'Database Error: unable to connect to the database due to:\n{err}')
+        return self.conn
 
-    def __exit__(self, exc_type, esc_val, exc_tb):
-        self.cursor.close()
+    def execute_query(self, query):
+        cursor = self.__enter__().cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+
+    def __exit__(self, cursor, exc_type, esc_val, exc_tb):
+        cursor.close()
         self.conn.close()
 
 
 if __name__ == "__main__":
-    with DataBaseConnector(db_name='countries', host='10.10.30.252', port='5432', user='postgres',
-                           password='p0S+gre') as a:
-        a.execute('SELECT a.name, a.population FROM "public"."Capital" as a WHERE '
-                  'a.population>10000000 LIMIT 10;')
-        print(a.fetchall())
-        a.execute('SELECT b.name FROM "public"."Country" as b WHERE b.population<50000 '
-                  'ORDER BY b.name DESC LIMIT 10;')
-        print(a.fetchall())
+    a = DataBaseConnector(db_name='countries', host='10.10.30.252', port='5432', user='postgres',
+                          password='p0S+gre')
+    print(a.execute_query('SELECT a.name, a.population FROM "public"."Capital" as a WHERE '
+                          'a.population>10000000 LIMIT 10;'))
+    print(a.execute_query('SELECT b.name FROM "public"."Country" as b WHERE b.population<50000 '
+                          'ORDER BY b.name DESC LIMIT 10;'))
