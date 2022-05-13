f1c45e0f062c350fde77e7f75b7b4d49d94ea270
Lidiia_Yen <lydyia.en@nixsolutions.com>
2022-05-13 16:47:27 +0300

    refactor_files

--- conftest.py
+++ conftest.py
@@ -2,7 +2,22 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--rounding_index', default=4)
+
+
+@pytest.fixture(scope='session')
+def get_rounding_index(request):
+    rounding_index = request.config.getoption('--rounding_index')
+    return rounding_index
+
+
 def pytest_configure(config):
+    config.addinivalue_line('markers', 'add: mark test to run tests only for Add operation')
+    config.addinivalue_line('markers', 'subtract: mark test to run tests only for Subtract operation')
+    config.addinivalue_line('markers', 'multiply: mark test to run only for Multiply operation')
+    config.addinivalue_line('markers', 'divide: mark test to run only for Divide operation')
+    config.addinivalue_line('markers', 'square_root: mark test to run only for Square_root operation')
     config.addinivalue_line('markers', 'order_pizza')
 
 
