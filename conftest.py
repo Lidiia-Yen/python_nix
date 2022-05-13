b88ca0b37eeeece99184d3d6ad8ec303653aa965
Lidiia_Yen <lydyia.en@nixsolutions.com>
2022-05-13 16:47:56 +0300

    refactor_files

--- conftest.py
+++ conftest.py
@@ -21,8 +21,8 @@ def pytest_configure(config):
     config.addinivalue_line('markers', 'order_pizza')
 
 
-# @pytest.fixture(scope='session')
-# def chrome_driver():
-#     driver = webdriver.Chrome('chromedriver.exe')
-#     yield driver
-#     driver.quit()
+@pytest.fixture(scope='session')
+def chrome_driver():
+    driver = webdriver.Chrome('chromedriver.exe')
+    yield driver
+    driver.quit()
